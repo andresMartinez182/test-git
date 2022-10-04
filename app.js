e1681ba9af00a58666fa3390b566a4e927de8794
andresMartinez182 <martinezamd21@gmail.com>
2022-10-04 12:44:51 -0300

    javascript agregado

---
+++ app.js
@@ -0,0 +1,9 @@
+const $h1 = document.getElementById("title");
+
+$h1.addEventListener('click', menssage);
+
+function menssage (e) {
+    alert("welcome to the jungle");
+}
+
+$h1.style.cursor = "pointer";
